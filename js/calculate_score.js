65d79903fe34f88254423e94eb8267fdf976691a
liukxaa <644306737@qq.com>
2018-04-23 11:40:23 +0800

    重构 把获取题目从显示的部分抽离

--- js/calculate_score.js
+++ js/calculate_score.js
@@ -8,21 +8,15 @@ document.getElementById('btn_calculate_score').addEventListener('click', functio
     document.getElementById('score').innerHTML = '得分：' + score;
 });
 
-document.getElementById('content').appendChild(add_blank_div());
-document.getElementById('content').appendChild(add_single_choice_div());
-document.getElementById('content').appendChild(add_multi_choices_div());
-document.getElementById('content').appendChild(add_judge_div());
-document.getElementById('content').appendChild(add_short_answer_div());
 /**
  * 填空题块.
  * @returns {HTMLElement}
  */
-function add_blank_div() {
+function add_blank_div(questions) {
     let blank_div = document.createElement('div');
     let h1 = document.createElement('h2');
     h1.innerHTML = '一、填空题（每空5分，共20分）';
     blank_div.appendChild(h1);
-    let questions = get_blank_questions();
     for (let i = 0; i < questions.length; i++) {
         let p = document.createElement('p');
         p.id = 'blank_' + i;
@@ -51,12 +45,11 @@ function add_blank_div() {
  * 单题块.
  * @returns {HTMLDivElement}
  */
-function add_single_choice_div() {
+function add_single_choice_div(questions) {
     let single_choice_div = document.createElement('div');
     let h2 = document.createElement('h2');
     h2.innerHTML = '二、选择题（每题10分，共20分）';
     single_choice_div.appendChild(h2);
-    let questions = get_single_choice_questions();
     for (let i = 0; i < questions.length; i++) {
         let p = document.createElement('p');
         p.id = 'single_choice_' + i;
@@ -77,12 +70,11 @@ function add_single_choice_div() {
  * 多选题块
  * @returns {HTMLDivElement}
  */
-function add_multi_choices_div() {
+function add_multi_choices_div(questions) {
     let multi_choices_div = document.createElement('div');
     let h2 = document.createElement('h2');
     h2.innerHTML = '三、多选题（每题10分，共20分）';
     multi_choices_div.appendChild(h2);
-    let questions = get_multi_choices_questions();
     for (let i = 0; i < questions.length; i++) {
         let p = document.createElement('p');
         p.id = 'multi_choices_' + i;
@@ -103,12 +95,11 @@ function add_multi_choices_div() {
  * 判断块
  * @returns {HTMLDivElement}
  */
-function add_judge_div() {
+function add_judge_div(questions) {
     let judge_div = document.createElement('div');
     let h2 = document.createElement('h2');
     h2.innerHTML = '四、判断题（每题10分，共20分）';
     judge_div.appendChild(h2);
-    let questions = get_judge_questions();
     for (let i = 0; i < questions.length; i++) {
         let p = document.createElement('p');
         p.id = 'judge_' + i;
@@ -125,12 +116,11 @@ function add_judge_div() {
  * 简答块
  * @returns {HTMLDivElement}
  */
-function add_short_answer_div() {
+function add_short_answer_div(questions) {
     let simple_answer_div = document.createElement('div');
     let h2 = document.createElement('h2');
     h2.innerHTML = '五、简答题（每题20分，共20分）';
     simple_answer_div.appendChild(h2);
-    let questions = get_short_answer_questions();
     for (let i = 0; i < questions.length; i++) {
         let p = document.createElement('p');
         p.id = 'simple_answer_' + i;
@@ -146,4 +136,9 @@ function add_short_answer_div() {
     return simple_answer_div
 }
 
+document.getElementById('content').appendChild(add_blank_div(get_blank_questions()));
+document.getElementById('content').appendChild(add_single_choice_div(get_single_choice_questions()));
+document.getElementById('content').appendChild(add_multi_choices_div(get_multi_choices_questions()));
+document.getElementById('content').appendChild(add_judge_div(get_judge_questions()));
+document.getElementById('content').appendChild(add_short_answer_div(get_short_answer_questions()));
 
